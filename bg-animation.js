65f9b05fbef5729fe9729c6dd92a196d5f955519
Aryanshiral <rjcaryan11sci1118@gmail.com>
2025-12-25 21:52:26 +0530

    bg-animation.js

--- bg-animation.js
+++ bg-animation.js
@@ -1,9 +1,11 @@
-anime({
-  targets: '#dashboard-particles circle',
-  translateY: [20, -120],
-  direction: 'alternate',
-  easing: 'easeInOutSine',
-  duration: 6000,
-  delay: anime.stagger(400),
-  loop: true
+window.addEventListener("load", () => {
+  anime({
+    targets: "#dust-paarticle path",
+    translateY: [20, -120],
+    direction: "alternate",
+    easing: "easeInOutSine",
+    duration: 6000,
+    delay: anime.stagger(400),
+    loop: true
+  });
 });
